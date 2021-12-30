79be580e50e1189bce2827426fed80d286d41428
Christian Bewernitz <coder@karfau.de>
2021-12-30 13:38:47 +0100

    refactor: Use less verbose `Deno.test` syntax

--- scor.test.ts
+++ scor.test.ts
@@ -15,6 +15,7 @@ import {
   setToValue,
   ToValue,
 } from "./scor.ts";
+import test = Deno.test;
 
 const assertReadonlyProperties = <Item>(
   score: Scor<Item>,
@@ -41,111 +42,92 @@ const assertReadonlyProperties = <Item>(
   }, TypeError);
   assertStrictEquals(score.max, MAX);
 };
-Deno.test({
-  name: "scor stores all explicit options",
-  fn: async (t) => {
-    type Item = { p: number };
-    const toValue: ToValue<Item> = (item) => item.p;
-    const MIN = 1;
-    const MAX = 5;
-    const options: AllOptions<Item> = { min: MIN, max: MAX, toValue };
-    const score = scor(options);
-    assertObjectMatch(
-      score,
-      options as unknown as Record<string, unknown>,
-    );
-    await t.step("modifying the options doesn't modify the Scor", () => {
-      options.min--;
-      assertStrictEquals(score.min, MIN);
+test("scor stores all explicit options", async (t) => {
+  type Item = { p: number };
+  const toValue: ToValue<Item> = (item) => item.p;
+  const MIN = 1;
+  const MAX = 5;
+  const options: AllOptions<Item> = { min: MIN, max: MAX, toValue };
+  const score = scor(options);
+  assertObjectMatch(
+    score,
+    options as unknown as Record<string, unknown>,
+  );
+  await t.step("modifying the options doesn't modify the Scor", () => {
+    options.min--;
+    assertStrictEquals(score.min, MIN);
 
-      options.min++;
-      assertStrictEquals(score.max, MAX);
+    options.min++;
+    assertStrictEquals(score.max, MAX);
 
-      options.toValue = () => 0;
-      assertStrictEquals(score.toValue, toValue);
-    });
-    assertReadonlyProperties(score, toValue, MIN, MAX);
-  },
+    options.toValue = () => 0;
+    assertStrictEquals(score.toValue, toValue);
+  });
+  assertReadonlyProperties(score, toValue, MIN, MAX);
 });
 
-Deno.test({
-  name: "not setting `min` or `max` option",
-  fn: async (t) => {
-    await t.step("when both are not set `forValue` throws", () => {
-      const score = scor({}); // not providing a range upfront is valid
-      assertThrows(() => score.forValue(0), RangeError, INVALID_RANGE); // but it doesn't allow `forValue`
-    });
-    await t.step("stores `toValue`", () => {
-      const toValue = (item: unknown[]) => item.length;
-      const score = scor({ toValue });
-      assertStrictEquals(score.toValue, toValue);
-      assertReadonlyProperties(score, toValue);
-    });
-    await t.step("when both are not set `forValue` throws", () => {
-      const score = scor({ min: 7 }); // only providing `min` is valid
-      assertThrows(() => score.forValue(0), RangeError, INVALID_RANGE); // but it doesn't allow `forValue`
-    });
-    await t.step("when both are not set `forValue` throws", () => {
-      const score = scor({ max: 7 }); // only providing `max` is valid
-      assertThrows(() => score.forValue(0), RangeError, INVALID_RANGE); // but it doesn't allow `forValue`
-    });
-  },
+test("not setting `min` or `max` option", async (t) => {
+  await t.step("when both are not set `forValue` throws", () => {
+    const score = scor({}); // not providing a range upfront is valid
+    assertThrows(() => score.forValue(0), RangeError, INVALID_RANGE); // but it doesn't allow `forValue`
+  });
+  await t.step("stores `toValue`", () => {
+    const toValue = (item: unknown[]) => item.length;
+    const score = scor({ toValue });
+    assertStrictEquals(score.toValue, toValue);
+    assertReadonlyProperties(score, toValue);
+  });
+  await t.step("when both are not set `forValue` throws", () => {
+    const score = scor({ min: 7 }); // only providing `min` is valid
+    assertThrows(() => score.forValue(0), RangeError, INVALID_RANGE); // but it doesn't allow `forValue`
+  });
+  await t.step("when both are not set `forValue` throws", () => {
+    const score = scor({ max: 7 }); // only providing `max` is valid
+    assertThrows(() => score.forValue(0), RangeError, INVALID_RANGE); // but it doesn't allow `forValue`
+  });
 });
 
-Deno.test({
-  name: "not setting `toValue`",
-  fn: async (t) => {
-    await t.step("with an empty range `forItem` returns 0", () => {
-      const score = scor({ min: 0, max: 0 }); // not providing `toValue` upfront is valid
-      assertStrictEquals(score.forItem(0), 0); // no need to call toValue, since the score is always 0
-    });
-    await t.step("with a range `forItem` throws", () => {
-      const score = scor({ min: 0, max: 1 }); // not providing `toValue` upfront is valid
-      assertThrows(() => score.forItem(0), Error); // but it doesn't allow `forItem`
-    });
-  },
+test("not setting `toValue`", async (t) => {
+  await t.step("with an empty range `forItem` returns 0", () => {
+    const score = scor({ min: 0, max: 0 }); // not providing `toValue` upfront is valid
+    assertStrictEquals(score.forItem(0), 0); // no need to call toValue, since the score is always 0
+  });
+  await t.step("with a range `forItem` throws", () => {
+    const score = scor({ min: 0, max: 1 }); // not providing `toValue` upfront is valid
+    assertThrows(() => score.forItem(0), Error); // but it doesn't allow `forItem`
+  });
 });
 
-Deno.test({
-  name: "min, max options can not be NaN",
-  fn: () => {
-    assertThrows(() => scor({ min: NaN }), RangeError, INVALID_RANGE);
-    assertThrows(() => scor({ max: NaN }), RangeError, INVALID_RANGE);
-  },
+test("min, max options can not be NaN", () => {
+  assertThrows(() => scor({ min: NaN }), RangeError, INVALID_RANGE);
+  assertThrows(() => scor({ max: NaN }), RangeError, INVALID_RANGE);
 });
 
-Deno.test({
-  name: "setting min > max option fails",
-  fn: () => {
-    assertThrows(() => scor({ min: 1, max: 0.999 }), RangeError, INVALID_RANGE);
-  },
+test("setting min > max option fails", () => {
+  assertThrows(() => scor({ min: 1, max: 0.999 }), RangeError, INVALID_RANGE);
 });
 
-Deno.test({
-  name: "setting min == max option",
-  fn: async (t) => {
-    await t.step("to 0, `forValue` is always 0", () => {
-      assertStrictEquals(scor({ min: 0, max: 0 }).forValue(0), 0);
-    });
-    await t.step("to positive values, `forValue` is always 0", () => {
-      assertStrictEquals(scor({ min: 1, max: 1 }).forValue(1), 0);
-    });
-    await t.step("to negative values `forValue` is always 0", () => {
-      assertStrictEquals(scor({ min: -1, max: -1 }).forValue(-1), 0);
-    });
-    await t.step("stores `toValue`", () => {
-      const toValue = (item: unknown[]) => item.length;
-      const score = scor({ min: 0, max: 0, toValue });
-      assertStrictEquals(score.toValue, toValue);
-      assertReadonlyProperties(score, toValue, 0, 0);
-    });
-  },
+test("setting min == max option", async (t) => {
+  await t.step("to 0, `forValue` is always 0", () => {
+    assertStrictEquals(scor({ min: 0, max: 0 }).forValue(0), 0);
+  });
+  await t.step("to positive values, `forValue` is always 0", () => {
+    assertStrictEquals(scor({ min: 1, max: 1 }).forValue(1), 0);
+  });
+  await t.step("to negative values `forValue` is always 0", () => {
+    assertStrictEquals(scor({ min: -1, max: -1 }).forValue(-1), 0);
+  });
+  await t.step("stores `toValue`", () => {
+    const toValue = (item: unknown[]) => item.length;
+    const score = scor({ min: 0, max: 0, toValue });
+    assertStrictEquals(score.toValue, toValue);
+    assertReadonlyProperties(score, toValue, 0, 0);
+  });
 });
 
-Deno.test({
-  name:
-    "when the range is between 0 `min` and positive `max`, `forValue` returns the expected value",
-  fn: () => {
+test(
+  "when the range is between 0 `min` and positive `max`, `forValue` returns the expected value",
+  () => {
     const score = scor({ min: 0, max: 100 });
     assertStrictEquals(score.forValue(-1), 0);
     assertStrictEquals(score.forValue(0), 0);
@@ -160,12 +142,11 @@ Deno.test({
     assertStrictEquals(score.forValue(101), 1);
     assertStrictEquals(score.forValue(NaN), 0);
   },
-});
+);
 
-Deno.test({
-  name:
-    "when the range is between positive `min` and positive `max`, `forValue` returns the expected value",
-  fn: () => {
+test(
+  "when the range is between positive `min` and positive `max`, `forValue` returns the expected value",
+  () => {
     const score = scor({ min: 100, max: 200 });
     assertStrictEquals(score.forValue(99), 0);
     assertStrictEquals(score.forValue(100), 0);
@@ -180,12 +161,11 @@ Deno.test({
     assertStrictEquals(score.forValue(201), 1);
     assertStrictEquals(score.forValue(NaN), 0);
   },
-});
+);
 
-Deno.test({
-  name:
-    "when the range is between negative `min` and `max` is 0, `forValue` returns the expected value",
-  fn: () => {
+test(
+  "when the range is between negative `min` and `max` is 0, `forValue` returns the expected value",
+  () => {
     const score = scor({ min: -100, max: 0 });
     assertStrictEquals(score.forValue(-101), 0);
     assertStrictEquals(score.forValue(-100), 0);
@@ -200,12 +180,11 @@ Deno.test({
     assertStrictEquals(score.forValue(1), 1);
     assertStrictEquals(score.forValue(NaN), 0);
   },
-});
+);
 
-Deno.test({
-  name:
-    "when the range is between negative `min` and negative `max`, `forValue` returns the expected value",
-  fn: () => {
+test(
+  "when the range is between negative `min` and negative `max`, `forValue` returns the expected value",
+  () => {
     const score = scor({ min: -200, max: -100 });
     assertStrictEquals(score.forValue(-201), 0);
     assertStrictEquals(score.forValue(-200), 0);
@@ -220,12 +199,11 @@ Deno.test({
     assertStrictEquals(score.forValue(-99), 1);
     assertStrictEquals(score.forValue(NaN), 0);
   },
-});
+);
 
-Deno.test({
-  name:
-    "when the range is between negative `min` and positive `max`, `forValue` returns the expected value",
-  fn: () => {
+test(
+  "when the range is between negative `min` and positive `max`, `forValue` returns the expected value",
+  () => {
     const score = scor({ min: -100, max: 100 });
     assertStrictEquals(score.forValue(-101), 0);
     assertStrictEquals(score.forValue(-100), 0);
@@ -240,29 +218,26 @@ Deno.test({
     assertStrictEquals(score.forValue(101), 1);
     assertStrictEquals(score.forValue(NaN), 0);
   },
+);
+
+test("setting `getValue` and a range allows calls to `forItem`", () => {
+  const toValue = (item: { p: number }) => item.p;
+  const score = scor({ min: 0, max: 100, toValue });
+  assertStrictEquals(score.forItem({ p: -1 }), 0);
+  assertStrictEquals(score.forItem({ p: 0 }), 0);
+  assertStrictEquals(score.forItem({ p: 1 }), 0.01);
+  assertStrictEquals(score.forItem({ p: 10 }), 0.1);
+  assertStrictEquals(score.forItem({ p: 25 }), 0.25);
+  assertStrictEquals(score.forItem({ p: 50 }), 0.5);
+  assertStrictEquals(score.forItem({ p: 74.9 }), 0.7490000000000001);
+  assertStrictEquals(score.forItem({ p: 99.99 }), 0.9998999999999999);
+  assertStrictEquals(score.forItem({ p: 99.999 }), 0.9999899999999999);
+  assertStrictEquals(score.forItem({ p: 100 }), 1);
+  assertStrictEquals(score.forItem({ p: 101 }), 1);
+  assertStrictEquals(score.forItem({ p: NaN }), 0);
 });
 
-Deno.test({
-  name: "setting `getValue` and a range allows calls to `forItem`",
-  fn: () => {
-    const toValue = (item: { p: number }) => item.p;
-    const score = scor({ min: 0, max: 100, toValue });
-    assertStrictEquals(score.forItem({ p: -1 }), 0);
-    assertStrictEquals(score.forItem({ p: 0 }), 0);
-    assertStrictEquals(score.forItem({ p: 1 }), 0.01);
-    assertStrictEquals(score.forItem({ p: 10 }), 0.1);
-    assertStrictEquals(score.forItem({ p: 25 }), 0.25);
-    assertStrictEquals(score.forItem({ p: 50 }), 0.5);
-    assertStrictEquals(score.forItem({ p: 74.9 }), 0.7490000000000001);
-    assertStrictEquals(score.forItem({ p: 99.99 }), 0.9998999999999999);
-    assertStrictEquals(score.forItem({ p: 99.999 }), 0.9999899999999999);
-    assertStrictEquals(score.forItem({ p: 100 }), 1);
-    assertStrictEquals(score.forItem({ p: 101 }), 1);
-    assertStrictEquals(score.forItem({ p: NaN }), 0);
-  },
-});
-
-Deno.test("`setMin` returns `Scor` with updated `min`", () => {
+test("`setMin` returns `Scor` with updated `min`", () => {
   const toValue = () => 0;
   const first = scor({ min: 0, max: 25, toValue });
   const second = setMin(first, 5);
@@ -272,7 +247,7 @@ Deno.test("`setMin` returns `Scor` with updated `min`", () => {
   assertStrictEquals(second.toValue, toValue);
 });
 
-Deno.test("`setMax` returns `Scor` with updated `min`", () => {
+test("`setMax` returns `Scor` with updated `min`", () => {
   const toValue = () => 0;
   const first = scor({ min: 0, max: 25, toValue });
   const second = setMax(first, 5);
@@ -282,7 +257,7 @@ Deno.test("`setMax` returns `Scor` with updated `min`", () => {
   assertStrictEquals(second.toValue, toValue);
 });
 
-Deno.test("`setRange` returns `Scor` with updated `min` and `max`", () => {
+test("`setRange` returns `Scor` with updated `min` and `max`", () => {
   const toValue = () => 0;
   const first = scor({ min: 0, max: 25, toValue });
   const second = setRange(first, 5, 10);
@@ -292,7 +267,7 @@ Deno.test("`setRange` returns `Scor` with updated `min` and `max`", () => {
   assertStrictEquals(second.toValue, toValue);
 });
 
-Deno.test("`setToValue` returns `Scor` with updated `toValue`", () => {
+test("`setToValue` returns `Scor` with updated `toValue`", () => {
   const toValue = () => 0;
   const first = scor({ min: 0, max: 25 });
   const second = setToValue(first, toValue);
